080c85d57f82430d9566fb8b40656f6dce15ef3c
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-06-15 12:58:52 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import NewHeader from './components/NewHeader.vue'
+</script>
+
+<template>
+  <NewHeader/>
+</template>
+
+<style lang="scss">
+@import './components/styles/general.scss'
+</style>
